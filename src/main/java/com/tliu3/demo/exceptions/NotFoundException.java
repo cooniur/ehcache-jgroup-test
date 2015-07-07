82ea8b6007337ea552072539a53b888896a27ef6
Tongliang Liu <tliu3@illumina.com>
2015-07-07 12:25:35 -0700

    Use correct status code for NotFoundException

--- src/main/java/com/tliu3/demo/exceptions/NotFoundException.java
+++ src/main/java/com/tliu3/demo/exceptions/NotFoundException.java
@@ -3,7 +3,7 @@ package com.tliu3.demo.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.CONFLICT)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 	private static final long serialVersionUID = -4593638799005145248L;
 
